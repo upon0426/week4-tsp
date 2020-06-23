2519b8628d0d3b9047d42b3a37ec32d6a913e553
upon0426 <karasawa.yui@gmail.com>
2020-06-23 22:06:55 +0900

    output in format

--- solver_YuiKarasawa.java
+++ solver_YuiKarasawa.java
@@ -1,21 +1,28 @@
 import java.util.*;
 import java.io.*;
 
+class SolverPoint {
+	int index;
+	double x;
+	double y;
+}
+
 public class solver_YuiKarasawa {
 
-	
 
 	
 	private static double calcDistance(double oldx, double oldy, double newx, double newy) {
 		return Math.sqrt((newx-oldx)*(newx-oldx) + (newy-oldy)*(newy-oldy));
 	}
 	
-    public static void main(String[] args) {
-        double[][] nums;
+	
+	private static List<SolverPoint> makeSpList(String fileName) {
+
         List<String> temp = new ArrayList<String>();
         
-        try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
+            
 
             while((line = br.readLine()) != null) {
                 temp.add(line);
@@ -25,32 +32,91 @@ public class solver_YuiKarasawa {
             e.printStackTrace();
         }
         
-        // delete the firstLine
-        //temp.remove(0);
         
-        nums = new double[2][temp.size()+1];
-        nums[0][0] = 0;
-        nums[1][0] = 0;
+        // define list of points
+        List<SolverPoint> spList = new ArrayList<SolverPoint>();
+         
+        // define start point
+        SolverPoint startPoint = new SolverPoint();
+        startPoint.index = 0;
+        startPoint.x = 0;
+        startPoint.y = 0;
+        
+        // add startPoint to beginning of List
+        spList.add(startPoint);
         
         for (int i=1;i<temp.size(); i++) {
-            String[] s = temp.get(i).split(",");
-            for (int j=0; j<2; j++) {
-                nums[j][i] = Double.parseDouble(s[j]);
-            }
+            SolverPoint sp = new SolverPoint();
+        	String[] s = temp.get(i).split(",");
+            
+            sp.index = i;
+            sp.x = Double.parseDouble(s[0]);
+            sp.y = Double.parseDouble(s[1]);
+            spList.add(sp);
         }
-        
-        nums[0][temp.size()] = 0;
-        nums[1][temp.size()] = 0;
-
-        ArrayList<Double> distances = new ArrayList<Double>();
+        // add startPoint to end of List
+        spList.add(startPoint);
+		
+		return spList;
+	}
+	
+	
+	private static double calcDistances(List<SolverPoint> spList) {
+        double sum = 0; 
+    	
         // for ‚Ì‚È‚©‚É‚¢‚ê‚é
-        for (int i=0; i<temp.size(); i++) {
-            distances.add(calcDistance(nums[0][i], nums[1][i], nums[0][i+1], nums[1][i+1]));
+        for (int i=0; i<spList.size()-1; i++) {
+            sum += calcDistance(spList.get(i).x, spList.get(i).y, spList.get(i+1).x, spList.get(i+1).y);
         }
-        
-        
-        for (int i=0; i<distances.size();i++) {
-        	System.out.println(distances.get(i));
+		return sum;
+	}
+	
+	
+    public static void main(String[] args) {
+    	
+    	// make list of points by input
+    	List<SolverPoint> spList = makeSpList(args[0]);
+    	
+    	// ---somehow change the order---
+    	// 1. find the closest
+//    	List<SolverPoint> spList = new ArrayList<SolverPoint>();
+//        // define start point
+//        SolverPoint startPoint = new SolverPoint();
+//        startPoint.index = 0;
+//        startPoint.x = 0;
+//        startPoint.y = 0;
+//        
+//        // add startPoint to beginning of List
+//        spList.add(startPoint);
+//    	double minDistance = calcDistance(oldSpList.get(0).x, oldSpList.get(0).y, oldSpList.get(1).x, oldSpList.get(1).y);
+//    	int minIndex;
+//    	double distance;
+//    	
+//    	for (int i=0; i<oldSpList.size()-2; i++) {
+//    		minIndex = 0;
+//    		distance = 0;
+//    		for (int j=1;j<oldSpList.size()-2; j++) {
+//    			distance = calcDistance(oldSpList.get(i).x, oldSpList.get(i).y, oldSpList.get(i+1).x, oldSpList.get(i+1).y);
+//    			if (distance < minDistance) {
+//    				minIndex = j;
+//    			}
+//    		}
+//    		SolverPoint p = new SolverPoint();
+//    		p.index = minIndex;
+//    		p.x = oldSpList.get(minIndex).x;
+//    		p.y = oldSpList.get(minIndex).y;
+//    		spList.add(p);
+//    	}
+//    	
+//    	spList.add(startPoint);
+    	
+    	
+    	// Calcurate all of distances
+    	//double totalDistances = calcDistances(spList);
+    	
+    	System.out.println("Index");
+        for (int i=1; i<spList.size()-1; i++) { // exclude start point & final destination
+        	System.out.println(spList.get(i-1).index);
         }
         
         
